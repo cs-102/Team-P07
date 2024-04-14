430062a25d4f57f447406877ac8380f9a3b084f6
Thunkoju-Abhilash <158248805+Thunkoju-Abhilash@users.noreply.github.com>
2024-04-15 01:00:08 +0530

    uploaded my part

--- Responsive restaurant website/js/script.js
+++ Responsive restaurant website/js/script.js
@@ -10,32 +10,3 @@ menuBtn.addEventListener("click",function(){
 closeBTN.addEventListener("click",function(){
   menus.classList.remove("active");
 })
-
-//--------------Swiper JS Code
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints:{
-    0:{
-      slidesPerView: 1,
-    },
-    768:{
-      slidesPerView: 2,
-    },
-    968:{
-      slidesPerView: 3,
-    },
-  }
-}) ;
